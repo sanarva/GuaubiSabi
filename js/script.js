ba456757bcac08fedf547ad547df246b61588b88
Sandra Arcas <plosky21@hotmail.com>
2021-08-14 17:06:15 +0200

    Modificaciones para mejora de la validación del formulario de contacto

--- js/script.js
+++ js/script.js
@@ -1,32 +1,82 @@
 $(document).ready(function(){
 
     $('#btnSend').click(function(){
-
+        var totalErrores = 0;
         var errores = '';
 
         // Validado Nombre ==============================
         if( $('#names').val() == '' ){
             errores += '<p>Escribe tu nombre</p>';
+            totalErrores++;
+            $('#names').css("border-bottom-color", "#F14B4B")
+        } else if(!validateText($('#names').val())){
+            errores += '<p>Escriba un nombre correcto. Se requiren un mínimo de tres letras. Ej. Ana</p>';
+            totalErrores++;
             $('#names').css("border-bottom-color", "#F14B4B")
         } else{
             $('#names').css("border-bottom-color", "#d1d1d1")
         }
 
+        
+        //Comprueba que el campo nombre y asunto contiene solo como mínimo, tres letras antes de enviar la petición al servidor
+        function validateText(names) {
+            const regexEmail = /^[A-Za-záéíóúàèìòùñçÁÉÍÓÚÀÈÌÒÙÑÇ\s'-]{3,50}$/;
+            return regexEmail.test(names) ? true : false;
+        }
+
         // Validado Correo ==============================
         if( $('#email').val() == '' ){
             errores += '<p>Escribe tu email</p>';
+            totalErrores++;
+            $('#email').css("border-bottom-color", "#F14B4B")
+        } else if(!validateFormatEmail($('#email').val())){
+            errores += '<p>El formato del email no es correcto. Ej. email@gmail.com</p>';
+            totalErrores++;
             $('#email').css("border-bottom-color", "#F14B4B")
         } else{
             $('#email').css("border-bottom-color", "#d1d1d1")
         }
 
+        //Comprueba que el campo email tiene un formato correcto antes de enviar la petición al servidor
+        function validateFormatEmail(email) {
+            const regexEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;    
+            return regexEmail.test(email) ? true : false;
+        }
+    
+            
+        // Validado Asunto ==============================
+        if( $('#subject').val() == '' ){
+            errores += '<p>Escribe un asunto</p>';
+            totalErrores++;
+            $('#subject').css("border-bottom-color", "#F14B4B")
+        } else if(!validateText($('#subject').val())){
+            errores += '<p>Escriba un asunto válido. Mínimo tres letras. No se permiten números ni caracteres especiales.</p>';
+            totalErrores++;
+            $('#subject').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#subject').css("border-bottom-color", "#d1d1d1")
+        }
+
         // Validado Mensaje ==============================
         if( $('#mensaje').val() == '' ){
             errores += '<p>Escribe tu mensaje</p>';
+            totalErrores++;
+            $('#mensaje').css("border-bottom-color", "#F14B4B")
+        } else if(!validateNoPubliWeb($('#mensaje').val())){
+            errores += '<p>Mensaje no válido. El mensaje debe tener entre 3 y 500 caracteres y no debe contener publicidad de ninguna web.</p>';
+            totalErrores++;
             $('#mensaje').css("border-bottom-color", "#F14B4B")
         } else{
             $('#mensaje').css("border-bottom-color", "#d1d1d1")
         }
+
+       
+        //Comprueba que el campo mensaje no contiene una dirección web. Para evitar spam
+        function validateNoPubliWeb(mensaje) {
+            const regexEmail = /^((?!http:)(?!HTTP:).){3,500}$/;
+            return regexEmail.test(mensaje) ? true : false;
+        }
+
 
         // ENVIANDO MENSAJE ============================
         if( errores == '' == false){
@@ -45,6 +95,13 @@ $(document).ready(function(){
         $('#btnClose').click(function(){
             $('.modal_wrap').remove();
         });
+
+        if(totalErrores > 0){
+            return false;
+        } else {
+            formularioContacto.action = "php/enviar.php"
+        }
+
     });
 
 });
--- php/enviar.php
+++ php/enviar.php
@@ -3,23 +3,37 @@
 // Llamando a los campos
 $nombre = $_POST['nombre'];
 $correo = $_POST['correo'];
-$telefono = $_POST['telefono'];
+$subject = $_POST['subject'];
 $mensaje = $_POST['mensaje'];
 
 // Datos para el correo
 $destinatario = "info@guaubisabi.com, $correo";
 $asunto = "Contacto desde web Guaubi Sabi";
 
-$carta = "Hola $nombre, \n \n";
-$carta .= "Este mensaje es para confirmarte que hemos recibido tu consulta:\n";
-$carta .= "     Asunto: $telefono\n";
-$carta .= "     Mensaje: $mensaje\n \n";
-$carta .= "Te contestaremos lo antes posible. Pero si tienes prisa, envianos un WhatsApp al 644 057 800\n \n";
-$carta .= "Saludos peludos\n \n";
+$emailBody = "
+<html>
+<body>
+  <p>Hola $nombre,</p>
+  <p>Este mensaje es para confirmarte que hemos recibido tu consulta:</p>
+  <p> · Asunto: $subject </p>
+  <p> · Mensaje: $mensaje </p>
+  <p>Te contestaremos lo antes posible. Pero si tienes prisa, envíanos un WhatsApp al 644 057 800</p>
+  <p>Saludos peludos.</p>
+  </body>
+</html>
+";
+
+// Para enviar un correo HTML, debe establecerse la cabecera Content-type
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+
+// Cabeceras adicionales
+$headers .= 'From: Guaubi Sabi <info@guaubisabi.com>' . "\r\n";
+
 
 
 // Enviando Mensaje
-mail($destinatario, $asunto, $carta);
+mail($destinatario, $asunto, $emailBody, $headers);
 header('Location:mensaje-de-envio.html');
 
 ?>
